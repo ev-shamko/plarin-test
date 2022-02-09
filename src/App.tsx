f960e34973478d46e1379395074782f99c8dbea3
ev-shamko <ev-shamko@yandex.ru>
2022-02-09 20:16:27 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Task } from './components/task';
+import './App.scss';
+
+const App = () => (
+  <div className="App">
+    <Task />
+  </div>
+);
+
+export default App;

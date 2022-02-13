9fb6ee5fa5d86457822a298bbc0b68f62f7f5971
ev-shamko <ev-shamko@yandex.ru>
2022-02-13 22:56:07 +0300

    added a grid container for house cards

--- src/components/task/task.tsx
+++ src/components/task/task.tsx
@@ -3,7 +3,7 @@ import './task.scss';
 import defaultStore from '../../stores/defaultStore';
 import { getPage } from '../../utils/api';
 import { observer } from 'mobx-react-lite';
-import { Pagination } from "@mui/material";
+import { Pagination, Grid } from "@mui/material";
 import { HouseCard } from './house-card/house-card';
 import { THouse } from '../../utils/types';
 
@@ -21,18 +21,37 @@ export const Task = observer(() => {
 
   return (
     <div className="task">
-      <a href={link} target="_blank" rel="noreferrer">
-        Задание
-      </a>
-      <ul>
-        {defaultStore.housesData.length > 0 && defaultStore.housesData.map((house: THouse, index: number) => (<HouseCard houseData={house} key={index} />))}
-      </ul>
+      <h1>Houses of Ice and Fire</h1>
 
       <Pagination
         count={pageQty}
         page={pageNumber}
         onChange={(_, num) => setPageNumber(num)}
+        sx={{ mb: 8, }}
       />
+
+      <a href={link} target="_blank" rel="noreferrer">
+        Задание
+      </a>
+      {/* <ul>
+        {defaultStore.housesData.length > 0 && defaultStore.housesData.map((house: THouse, index: number) => (<HouseCard houseData={house} key={index} />))}
+      </ul> */}
+
+      <Grid container spacing={{ xs: 2, md: 2 }} columns={{ xs: 2, sm: 8, md: 12 }} sx={{ mb: 8, mt: 8 }}>
+        {defaultStore.housesData.length > 0 && defaultStore.housesData.map((house: THouse, index: number) => (
+          <Grid item xs={2} sm={4} md={4} key={index}>
+            <HouseCard houseData={house} key={index} />
+          </Grid>
+        ))}
+      </Grid>
+
+      <Pagination
+        count={pageQty}
+        page={pageNumber}
+        onChange={(_, num) => setPageNumber(num)}
+        sx={{ mb: 8, }}
+      />
+
     </div>
   );
 });
